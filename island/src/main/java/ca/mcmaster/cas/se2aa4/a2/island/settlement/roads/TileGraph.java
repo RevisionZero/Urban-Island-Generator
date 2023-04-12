9acf3d377fd16a9039307c3952c2de50da1b487d
Omar Shehada <awadomar24@gmail.com>
2023-04-11 23:38:55 -0400

    F04: Split up the shortest path implementation: code refactoring

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/settlement/roads/TileGraph.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/settlement/roads/TileGraph.java
@@ -1,6 +1,8 @@
 package ca.mcmaster.cas.se2aa4.a2.island.settlement.roads;
 
+import ca.mcmaster.cas.se2aa4.a2.island.geography.Land;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileGroup;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.mesh.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.segment.Segment;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.vertex.Vertex;
@@ -16,68 +18,61 @@ public class TileGraph {
 
     private Mesh mesh;
 
-    private Set<Vertex> vertices;
+    private final Set<Vertex> verticesNodes;
 
-    private Set<Segment> segments;
+    private final Set<Edge<Vertex>> segmentEdges;
 
-    private Set<Edge<Vertex>> edges;
+    private final UndirectedGraph<Vertex> graph;
 
-    private UndirectedGraph<Vertex> graph;
+    private final Set<Tile> landTiles;
+
+    public Set<Tile> getLandTiles(){
+        return this.landTiles;
+    }
 
     /**
      * Constructs a new undirected graph connecting all the centroids of the given {@link Set} of {@link Tile}.
      * Each newly constructed {@link Segment} and its {@link Vertex} are added as invisible {@link Segment} and {@link Vertex} to the {@link Mesh}.
      * @param mesh The {@link Mesh} of the {@link Set} of {@link Tile}.
-     * @param tiles The {@link Set} of {@link Tile} to use to construct the undirected graph.
+     * @param land The {@link Set} of {@link Tile} to use to construct the undirected graph.
      */
-    public TileGraph(Mesh mesh, Set<Tile> tiles){
+    public TileGraph(Mesh mesh, Land land){
         this.mesh = mesh;
-        this.vertices = new HashSet<>();
-        this.segments = new HashSet<>();
-        this.edges = new HashSet<>();
+        this.verticesNodes = new HashSet<>();
+        this.segmentEdges = new HashSet<>();
+        this.landTiles = new HashSet<>();
 
-        tiles.forEach(tile -> {
-            vertices.add(tile.getPolygon().getCentroid());
-            tile.getNeighbors().forEach(neighbor -> {
-                Segment segment = new Segment(tile.getPolygon().getCentroid(), neighbor.getPolygon().getCentroid());
-                segment.setColor(new Color(0,0,0,0));
-                this.mesh.addSegment(segment);
-                segments.add(segment);
+        land.getTiles().stream().filter(tile -> tile.getType().getGroup().equals(TileGroup.LAND)).forEach(tile -> {
+            verticesNodes.add(tile.getPolygon().getCentroid());
+            landTiles.add(tile);
+            tile.getNeighbors().stream().filter(neighbor -> neighbor.getType().getGroup().equals(TileGroup.LAND)).forEach(neighbor -> {
                 double weight = (tile.getElevation() + neighbor.getElevation()) / 2;
-                edges.add(new UndirectedEdge<>(tile.getPolygon().getCentroid(), neighbor.getPolygon().getCentroid(), weight));
+                segmentEdges.add(new UndirectedEdge<>(tile.getPolygon().getCentroid(), neighbor.getPolygon().getCentroid(), weight));
             });
         });
         this.mesh.getVertices().forEach(vertex -> {
             vertex.setColor(new Color(0,0,0,0));
         });
 
-        graph = new UndirectedGraph<>(vertices, edges);
+        graph = new UndirectedGraph<>(verticesNodes, segmentEdges);
     }
 
-    /**
-     *
-     * @param source The source {@link Vertex} of the shortest path.
-     * @param target The {@link Vertex} node of the shortest path.
-     * @return An {@link Optional} describing the path between the souce and target as a {@link List} of {@link Segment}.
-     */
-    public Optional<List<Segment>> findShortestPath(Vertex source, Vertex target){
-        List<Segment> path = new ArrayList<>();
-        Optional<List<Vertex>> shortestPath = graph.findShortestPath(source, target);
+    public Map<Vertex, Vertex> tileGraphDijkstra(Vertex source) throws IllegalArgumentException{
+        return this.graph.dijkstra(source);
+    }
 
-        if(shortestPath.isPresent()){
-            List<Vertex> pathVertices = shortestPath.get();
+    public Optional<List<Vertex>> tileGraphExtractPath(Map<Vertex, Vertex> pathMap, Vertex source, Vertex target) throws IllegalArgumentException{
+        return this.graph.extractPath(pathMap, source, target);
+    }
 
-            for(int i = 0; i < pathVertices.size()-1; i++){
-                int finalI = i;
-                segments.forEach(segment -> {
-                    if(segment.equals(new Segment(pathVertices.get(finalI), pathVertices.get(finalI + 1)))){
-                        path.add(segment);
-                    }
-                });
-            }
+    public void drawRoad(List<Vertex> shortestPath){
+
+        for(int i = 0; i < shortestPath.size()-1; i++){
+            Segment segment = new Segment(shortestPath.get(i), shortestPath.get(i + 1));
+            segment.setColor(new Color(0,0,0));
+            this.mesh.addSegment(segment);
+
         }
-
-        return Optional.of(path);
     }
 
 }
